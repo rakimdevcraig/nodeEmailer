2d8ce64759c8d1f0161c73c32ee33b5455ff34ff
rakimdevcraig <rakimdevcraig@gmail.com>
2020-01-15 23:35:26 -0500

    Updated SMTP provider

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const nodemailer = require('nodemailer');
-// let transport = nodemailer.createTransport(options[defaults])
 
-let transport = nodemailer.createTransport({
-    host: 'smtp.mailtrap.io',
-    port: 2525,
-    auth: {
-        user: 'd025b14c5841a4',
-        pass: '87551db675d23c'
-    }
-});
+function sendEmail() {
 
-const message = {
-    from: 'elonmusk@gmail.com', // Sender address
-    to: 'rakimcraig617@gmail.com',         // List of recipients
-    subject: 'Price for the macbook has dropped below your target!!', // Subject line
-    text: 'Go and get your macbook at its new low price!!' // Plain text body
-};
-transport.sendMail(message, function (err, info) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(info);
-    }
-});
+    let transport = nodemailer.createTransport({
+        host: 'in-v3.mailjet.com',
+        port: 587,
+        auth: {
+            user: 'ff586614ece0c0035d77994550637bc6',
+            pass: '01f9a15ca9bb288d7e3529137b532e01'
+        }
+    });
+
+    const message = {
+        from: 'rakimdevcraig@gmail.com', // Sender address
+        to: 'rakimcraig617@gmail.com',         // List of recipients
+        subject: 'Price for the macbook has dropped below your target!!', // Subject line
+        text: 'Go and get your macbook at its new low price!!' // Plain text body
+    };
+    transport.sendMail(message, function (err, info) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(info);
+        }
+    });
+}
+sendEmail()
